04fc196148b2bdde49495ba53af2b346287d26be
Roonder <107264752+Roonder@users.noreply.github.com>
2022-08-16 15:57:35 -0400

    Segundo Proyecto - Upload files

---
+++ Segundo_Proyecto/index.js
@@ -0,0 +1,10 @@
+const name = "Juliam";
+const age = 22;
+const dev = true;
+const birthDate = new Date(1999, 8, 9);
+const favBook = {
+    title:'Percy Jackson, El Último Héroe del Olimpo',
+    author: 'Rick Riordan',
+    date: 2009,
+    url: 'https://es.wikipedia.org/wiki/El_%C3%BAltimo_h%C3%A9roe_del_Olimpo'
+}
